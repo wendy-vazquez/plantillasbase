6c1719f26e283f62e4cd801fe996c471fefe4572
Wendy-Vazquez <23308060610440@cetis61.edu.mx>
2025-10-07 21:18:51 -0600

    xd

--- app.py
+++ app.py
@@ -4,4 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('base.html')
+
+@app.route('/animales_exoticos')
+def aniexo():
+    return render_template('aniexo.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
